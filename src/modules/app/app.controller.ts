3bf1c6e2faa3b8e6bc5066ca88c6c9d0531a16e1
ahd3r <ander11110@gmail.com>
2022-07-01 01:23:10 +0300

    test new stuff

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
   getHello(): { data: string } {
     return { data: 'test' };
+  }
+
+  @Get('/test-get')
+  testReceive(): { data: string } {
+    return { data: 'test get' };
+  }
+
+  @Post('/test-post')
+  testCreate(@Body() data: any): { data: string } {
+    return { data: JSON.stringify(data) };
   }
 }
